43ae608a64fe87fda20cbe6f6878010efebb9b31
Musthofa Ali Ubaed <panic.inc.dev@gmail.com>
2023-03-26 23:07:45 +0700

    Update

--- app/src/main/java/com/paniclabs/rch/sample/MainActivity.kt
+++ app/src/main/java/com/paniclabs/rch/sample/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.paniclabs.rch.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val isEnable = rch.isEnabled()
+        Toast.makeText(this, "IsEnable $isEnable", Toast.LENGTH_SHORT).show()
     }
 }
